1d7a1bbfc7a31210716c86c7b66d2905b89097e3
Vahid1919 <vahidmenu@gmail.com>
2023-10-07 12:16:18 +0200

    feat: some typescript configuration

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+
+
+// const path = '/mnt/c/Users/vahid/Videos\Captures';
+// const directory = process.cwd();
+// console.log(directory)
+// // Videos\Captures
+
+
+import fs from 'fs';
+console.log("Hello World")
+// const path = '/mnt/c/Users/vahid/Videos\Captures';
+
+// const didntWork = () => console.log("doesn't exist")
+
+
+// access(directory, didntWork)
